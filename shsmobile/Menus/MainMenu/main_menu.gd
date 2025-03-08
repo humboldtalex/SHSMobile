5b3892dc062f6ca5192f5d70cf50d7ba0dc4b889
Nexagonal <nexagonal@gmail.com>
2025-03-08 11:48:49 -0800

    Laid Down Foundation for Stuff

---
+++ shsmobile/Menus/MainMenu/main_menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+@onready var start_button: Button = $VBoxContainer/StartButton
+@onready var options_button: Button = $VBoxContainer/OptionsButton
+@onready var credits_button: Button = $VBoxContainer/CreditsButton
+
+func _ready():
+	credits_button.connect("pressed", on_credits_pressed)
+
+func _process(delta):
+	pass
+
+func on_credits_pressed():
+	get_tree().change_scene_to_file("res://Menus/Credits/credits.tscn")
