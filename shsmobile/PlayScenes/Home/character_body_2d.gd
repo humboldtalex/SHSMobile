78aa7cb9bf1fd656696eeec67964dee1f585a7f1
Nexagonal <nexagonal@gmail.com>
2025-03-08 21:01:01 -0800

    Keeping character in bounds in home scene

--- shsmobile/PlayScenes/Home/character_body_2d.gd
+++ shsmobile/PlayScenes/Home/character_body_2d.gd
@@ -1,6 +1,9 @@
 extends CharacterBody2D
 
 @export var speed = 600
+@export var min_bound : Vector2 = Vector2(175, 560)
+@export var max_bound : Vector2 = Vector2(900, 2400)
+
 @onready var animation_player: AnimationPlayer = $SubViewportContainer/SubViewport/AnimalModel/AnimationPlayer
 @onready var animal_model: Node3D = $SubViewportContainer/SubViewport/AnimalModel
 
@@ -10,7 +13,8 @@ var target_position = Vector2()
 func _physics_process(delta):
 	if Input.is_action_just_pressed("click"):
 		click_position = get_global_mouse_position()
-		print(click_position)
+		click_position.x = clampf(click_position.x, min_bound.x, max_bound.x)
+		click_position.y = clampf(click_position.y, min_bound.y, max_bound.y)
 		
 	if position.distance_to(click_position) > 10:
 		target_position = (click_position - position).normalized()
