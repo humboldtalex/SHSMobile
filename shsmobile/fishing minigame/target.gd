d57d8a9593428a737c6aabd97d6cb188bfa77025
David Hungerman <dmhungerman@gmail.com>
2025-03-09 08:47:55 -0700

    Fishing Polish

--- shsmobile/fishing minigame/target.gd
+++ shsmobile/fishing minigame/target.gd
@@ -1,6 +1,6 @@
 extends Area2D
 @onready var fish_line: Line2D = $FishLine
-
+@onready var sprite_2d: Sprite2D = $Sprite2D
 signal target_entered()
 signal target_exited()
 
@@ -13,9 +13,12 @@ var direction := Vector2.ZERO
 var on_fish := false
 var delay : float = 1
 var respawn : bool = false
+var random_vector : Vector2 = Vector2.ZERO
+
 
 func _ready() -> void:
 	START_POS = global_position
+	fish_line.visible = false
 
 func _physics_process(delta: float) -> void:
 	_check_on_fish()
@@ -34,23 +37,41 @@ func _physics_process(delta: float) -> void:
 			direction = (end_position - start_position)
 			speed = direction.length() / time
 			direction = direction.normalized()
+			time = 0
+			sprite_2d.rotation = direction.angle()
+			fish_line.visible = true
+			
+			#fish_line.cr
 	
 func _process(delta):
 	time = time + delta
 	if speed > 0:
 		global_position += direction * speed * delta / 2 	#Moves target
-		speed -= 7000 * delta								#Target deceleration
-		
+		speed -= 7000 * delta								#Target decceleration
+		if time > 0.025:
+			time = 0
+			var r = speed / 125
+			random_vector = Vector2(randf_range(-r,r),randf_range(-r,r))
+			fish_line.add_point(START_POS - fish_line.global_position + random_vector)
+			sprite_2d.scale -= Vector2(.005,.005)
 		#print(speed)
 		if speed < 50:		#Speed cutoff, below this point, target 'hits the water'
 			speed = 0
 			respawn = true
+			sprite_2d.visible=false
 	if respawn == true:		#countdown before respawning target
 		delay -= delta
 		if delay < 0:
 			delay = 1
 			respawn = false
+			var line_len : int = fish_line.get_point_count()
+			fish_line.visible = false
 			global_position = START_POS
+			sprite_2d.scale = Vector2(.25,.25)
+			sprite_2d.visible=true
+
+			for i in line_len-2:
+				fish_line.remove_point(2)
 	
 func _check_on_fish() -> void:
 	var bodies := get_overlapping_bodies()
